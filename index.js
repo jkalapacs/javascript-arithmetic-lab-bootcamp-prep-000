5751cd12df71e9b0c028eadb24928acbcbe7046f
Sandor Kis <kissandor@hotmail.com>
2018-01-25 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(a,b){
-  return a+b
+  return a + b
+}
+
+function substract(a,b){
+  return a - b
+}
+
+function multiplie(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
 }
